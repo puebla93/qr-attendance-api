f3f7dcee884785b6af7f51c1d19e3182ebd8b7aa
Javier Puebla <jpuebla1993@gmail.com>
2020-05-18 19:43:14 -0400

    add Students model

--- attendance/models.py
+++ attendance/models.py
@@ -2,11 +2,8 @@ from django.db import models
 
 
 class Attendances(models.Model):
-    # student id
-    student_id = models.CharField(max_length=255, null=False)
-
-    # student name
-    student_name = models.CharField(max_length=255, null=False)
+    # student
+    student = models.ForeignKey(Students)
 
     # teacher name
     teacher = models.CharField(max_length=255, null=False)
@@ -25,3 +22,11 @@ class Attendances(models.Model):
 
     def __str__(self):
         return "{} - {}:{} - {}".format(self.student_name, self.class_type, self.course_name, self.date)
+
+
+class Students(models.Model):
+    # student id
+    student_id = models.CharField(max_length=255, null=False)
+
+    # student name
+    student_name = models.CharField(max_length=255, null=False)
