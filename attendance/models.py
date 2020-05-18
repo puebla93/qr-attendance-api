d218470241f868fefa7ef47fc60d93cba3d87efe
Javier Puebla <jpuebla1993@gmail.com>
2020-05-18 19:55:52 -0400

    adding Teachers, ClassTypes and Courses models

--- attendance/models.py
+++ attendance/models.py
@@ -1,32 +1,59 @@
 from django.db import models
 
 
+class Students(models.Model):
+    # student id
+    id = models.CharField(max_length=255, null=False, primary_key=True)
+
+    # student name
+    name = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return "N:{} CI:{}".format(self.name, self.id)
+
+
+class Teachers(models.Model):
+    # teacher name
+    name = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class ClassTypes(models.Model):
+    # class type (eg. Practical Lesson, Conference)
+    class_type = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return self.class_type
+
+
+class Courses(models.Model):
+    # course name (eg. Programming, Artificial Intelligence)
+    course_name = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return self.course_name
+
+
 class Attendances(models.Model):
-    # student
+    # the student
     student = models.ForeignKey(Students)
 
-    # teacher name
-    teacher = models.CharField(max_length=255, null=False)
+    # the class teacher
+    teacher = models.ForeignKey(Teachers)
 
     # class date
     date = models.DateField(editable=False)
 
     # course name (eg. Programming, Artificial Intelligence)
-    course_name = models.CharField(max_length=255, null=False)
+    course_name = models.ForeignKey(ClassTypes)
 
     # class type (eg. Practical Lesson, Conference)
-    class_type = models.CharField(max_length=255, null=False)
+    class_type = models.ForeignKey(ClassTypes)
 
     # class details (eg. Last Practical Lesson, First Conference)
     details = models.CharField(max_length=255, null=True)
 
     def __str__(self):
-        return "{} - {}:{} - {}".format(self.student_name, self.class_type, self.course_name, self.date)
-
-
-class Students(models.Model):
-    # student id
-    student_id = models.CharField(max_length=255, null=False)
-
-    # student name
-    student_name = models.CharField(max_length=255, null=False)
+        return "{} - {}:{} - {}".format(self.student, self.class_type, self.course_name, self.date)
