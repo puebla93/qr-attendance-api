bd93737f46fa8277b4c67edee12a887f1bffebc9
Javier Puebla <jpuebla1993@gmail.com>
2020-05-18 20:09:25 -0400

    register all models in admin

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Attendances
+from .models import *
 
+admin.site.register(Students)
+admin.site.register(Teachers)
+admin.site.register(ClassTypes)
+admin.site.register(Courses)
 admin.site.register(Attendances)
