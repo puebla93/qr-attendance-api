1c293d79fff25ac1f4b227994db581333d4aecdc
Javier Puebla <jpuebla1993@gmail.com>
2020-05-18 20:07:42 -0400

    adding urls for all views

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import ListAttendancesView
+from .views import *
 
 
 urlpatterns = [
+    path('students/', ListStudentsView.as_view(), name="students-all"),
+    path('teachers/', ListTeachersView.as_view(), name="teachers-all"),
+    path('class_types/', ListClassTypesView.as_view(), name="class_types-all"),
+    path('courses/', ListCoursesView.as_view(), name="courses-all"),
     path('attendances/', ListAttendancesView.as_view(), name="attendances-all")
 ]
