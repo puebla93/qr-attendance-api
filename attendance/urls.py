ab998bfad60902f0c5139c9e46dc6bd694ab5bb8
Javier Puebla <jpuebla1993@gmail.com>
2020-05-20 22:04:04 -0400

    add 'auth/login/' url path in api/v1

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+
 from .views import *
 
-
 urlpatterns = [
+    path('auth/login/', LoginView.as_view(), name="auth-login"),
     path('students/', ListStudentsView.as_view(), name="students-all"),
     path('teachers/', ListTeachersView.as_view(), name="teachers-all"),
     path('class_types/', ListClassTypesView.as_view(), name="class_types-all"),
