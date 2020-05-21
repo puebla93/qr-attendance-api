58fc9b843950a780b28cf9507499e87a0227638d
Javier Puebla <jpuebla1993@gmail.com>
2020-05-21 18:20:00 -0400

    adding IsTeacherUser permission class

--- attendance/permissions.py
+++ attendance/permissions.py
@@ -8,3 +8,12 @@ class ReadOnly(BasePermission):
 
     def has_permission(self, request, view):
         return request.method in SAFE_METHODS
+
+
+class IsTeacherUser(BasePermission):
+    """
+        Allows access only to teacher users.
+    """
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated)
