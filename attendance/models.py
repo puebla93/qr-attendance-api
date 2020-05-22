b8f820981846386e2b583d21f7fbea6b187d64be
Javier Puebla <jpuebla1993@gmail.com>
2020-05-22 16:47:19 -0400

    deleting Users model

--- attendance/models.py
+++ attendance/models.py
@@ -2,26 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Users(User):
-    TEACHERS_EMAIL_ADDRESS = '@matcom.uh.cu'
-
-    @staticmethod
-    def is_valid_teacher_email(teacher_email):
-        return teacher_email.endswith(Users.TEACHERS_EMAIL_ADDRESS)
-
-    @staticmethod
-    def is_valid_student_id(student_id):
-        from datetime import datetime
-        try:
-            datetime(int("19"+student_id[:2]), int(student_id[2:4]), int(student_id[4:6]))
-        except ValueError:
-            try:
-                datetime(int("20"+student_id[:2]), int(student_id[2:4]), int(student_id[4:6]))
-            except ValueError:
-                return False
-
-        return True
-
 class ClassTypes(models.Model):
     # class type (eg. Practical Lesson, Conference)
     class_type = models.CharField(max_length=255, null=False, unique=True)
@@ -43,10 +23,10 @@ class Courses(models.Model):
 
 class Attendances(models.Model):
     # the student
-    student = models.ForeignKey(Users, on_delete=models.CASCADE)
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
 
     # the class teacher
-    teacher = models.ForeignKey(Users, on_delete=models.DO_NOTHING, related_name='teacher')
+    teacher = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='teacher')
 
     # class date
     date = models.DateField(editable=False)
