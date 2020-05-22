7c2aa404b948c324f37dee714478d20ca62b3613
Javier Puebla <jpuebla1993@gmail.com>
2020-05-21 21:41:56 -0400

    deleting admin registretion of Students and Teachers models

--- attendance/admin.py
+++ attendance/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Students)
-admin.site.register(Teachers)
 admin.site.register(ClassTypes)
 admin.site.register(Courses)
 admin.site.register(Attendances)
