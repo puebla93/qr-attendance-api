9e99856f3bbe86c606c00444033f89f4d9e16540
Javier Puebla <jpuebla1993@gmail.com>
2020-05-22 22:06:46 -0400

    use only birthday numbers from student id in is_valid_student_id util function

--- attendance/util.py
+++ attendance/util.py
@@ -5,11 +5,13 @@ def is_valid_teacher_email(teacher_email):
 
 def is_valid_student_id(student_id):
     from datetime import datetime
+
+    birthday = student_id[:6]
     try:
-        datetime(int("19"+student_id[:2]), int(student_id[2:4]), int(student_id[4:6]))
+        datetime(int("19"+birthday[:2]), int(birthday[2:4]), int(birthday[4:6]))
     except ValueError:
         try:
-            datetime(int("20"+student_id[:2]), int(student_id[2:4]), int(student_id[4:6]))
+            datetime(int("20"+birthday[:2]), int(birthday[2:4]), int(birthday[4:6]))
         except ValueError:
             return False
 
