a81c30f53438ebec6af31aed6dbec1564676e9af
Javier Puebla <jpuebla1993@gmail.com>
2020-05-23 16:19:55 -0400

    update ListCreateClassTypesView url

--- attendance/urls.py
+++ attendance/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('auth/login/', LoginView.as_view(), name="auth-login"),
     path('auth/register/', RegisterUsersView.as_view(), name="auth-register"),
 
-    path('class_types/', ListClassTypesView.as_view(), name="class_types-all"),
+    path('class_types/', ListCreateClassTypesView.as_view(), name="class_types-list-create"),
     path('class_types/<str:type>/', ClassTypesDetailView.as_view(), name="class_types-detail"),
 
     path('courses/', ListCoursesView.as_view(), name="courses-all"),
