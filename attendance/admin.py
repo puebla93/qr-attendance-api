3bee3f3f38ce94f681533c83f225108be34792da
Javier Puebla <jpuebla1993@gmail.com>
2020-05-24 12:06:23 -0400

    change default auth user model to Users

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import *
 
 admin.site.register(ClassTypes)
 admin.site.register(Courses)
-admin.site.register(Users)
+admin.site.register(Users, UserAdmin)
 admin.site.register(Attendances)
