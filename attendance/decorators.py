e11f0f4e4d170d07a95674249bd96b236c13e41a
Javier Puebla <jpuebla1993@gmail.com>
2020-05-24 18:40:53 -0400

    update imports

--- attendance/decorators.py
+++ attendance/decorators.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import status
+
 from .models import Users
 
 
--- attendance/models.py
+++ attendance/models.py
@@ -1,6 +1,6 @@
-from django.db import models
-from django.contrib.auth.models import User
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.db import models
 
 
 class Users(User):
